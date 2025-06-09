bcdd8eea2e4cc7e3cd7798783231c95e8e60997d
miguelltmac <102394529+miguelltmac@users.noreply.github.com>
2025-06-09 10:51:09 -0300

    Merge_Arte

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,11 @@
 extends CharacterBody2D
 
+	
+@export var speed : float = 100.0
+@export var jump_velocity : float = -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+
 const BULLET = preload("res://Scenes/MagicBall.tscn")
 
 var shoot_marker : Marker2D
@@ -10,25 +13,43 @@ var shoot_marker : Marker2D
 func _ready() -> void:
 	shoot_marker = $Marker2D
 	
-	
 func _physics_process(delta: float) -> void:
-	print(shoot_marker.position)
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("up") and is_on_floor():
+		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	var sprite : Sprite2D = $Sprite2D as Sprite2D
+	if velocity.x < 0:
+		sprite.flip_h = true
+	if velocity.x > 0:
+		sprite.flip_h = false
+		
+	if Input.is_action_just_pressed("shoot"):
+		pass
+	
+	if velocity.y < 0:
+		animation_player.play("Jump")
+	elif not is_on_floor():
+		animation_player.play("Fall")
+	elif is_on_floor() and velocity.x:
+		animation_player.play("Walk")
+	else :
+		animation_player.play("Idle")
 
+#Código do projétil
+	print(shoot_marker.position)
 	bullet_follow_mouse()
 	fire_bullet()
 	move_and_slide()
